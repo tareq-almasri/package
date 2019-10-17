719252c774d88857800b3ad2a05802491a20be25
tareq-almasri <talmasreq@gmail.com>
2019-10-17 14:10:37 +0200

    cabbage the node package

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports.sumOfTwoNumbers=(num1,num2)=>{
+    console.log(`the sum is ${num1+num2}`);
+}
+
+module.exports.productOfTwoNumbers = (num1, num2) => {
+    console.log(`the product is ${num1 * num2}`);
+}
